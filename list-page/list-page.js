1fea90e3c4a2b686c3bf311dd55130e27afa93d6
austinbhan <austinbhan@outlook.com>
2022-05-24 14:54:55 -0600

    Workshop titles showing in HTML, but tdd test returning Script Error

--- list-page/list-page.js
+++ list-page/list-page.js
@@ -1,11 +1,29 @@
 import { getWorkshops, logout } from '../fetch-utils.js';
 
-const workShopList = document.querySelector('workshop-list');
+const workShopsContainer = document.querySelector('.workshops-container');
 const logOut = document.getElementById('logout');
 
 logOut.addEventListener('click', () => {
     logout();
 });
 
-async function renderWorkshop() {
+export async function renderWorkshops() {
+    workShopsContainer.textContent = '';
+    const data = await getWorkshops();
+
+    for (let workshop of data) {
+        const workShopDiv = document.createElement('div');
+        workShopDiv.setAttribute('class', 'workshop');
+
+        const h3 = document.createElement('h3');
+        h3.setAttribute('class', 'workshop-name');
+        h3.textContent = workshop.name;
+
+        const ul = document.createElement('ul');
+        ul.setAttribute('class', 'participants');
+
+        workShopsContainer.append(workShopDiv);
+        workShopDiv.append(h3, ul);
+    }
 }
+renderWorkshops();
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,17 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderWorkShops } from '../list-page/list-page.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="workShopDiv"><h3 class="workshop-name">Workshop name</h3><ul class="participants"></ul></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderWorkShops();
 
     //Expect
     // Make assertions about what is expected versus the actual result
