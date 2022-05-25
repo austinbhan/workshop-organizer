e9283aa11dc6104de020169e9fd54bb6042f01f8
austinbhan <austinbhan@outlook.com>
2022-05-24 21:22:19 -0600

    Created form event listener

--- create-page/create-page.js
+++ create-page/create-page.js
@@ -9,10 +9,12 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const participantForm = new FormData(form);
     console.log(participantForm.get('create-name'));
 
-    await createParticipant({}); // Continue from Here
+    await createParticipant({ name: participantForm.get('create-name'), contact_info: participantForm.get('choose-workshop') });
+    window.location('../list-page');
+    form.reset();
 });
